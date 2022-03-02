f1fadcea42ab6fb06dd65805ef73bfac795e693f
caroline223 <67647836+caroline223@users.noreply.github.com>
2022-03-01 23:33:51 -0500

    Added the Effect.js component to the application

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import StateComponent from './Components/State';
+import EffectComponent from './Components/Effect';
 
 function App() {
   return (
     <div className="App">
      <h1>React Page</h1>
      <StateComponent />
+     <EffectComponent />
     </div>
   );
 }
---
+++ hooks/src/Components/Effect.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+
+
+function EffectComponent(){
+
+    const [time, setTime] = useState(new Date())
+    const [isRed, setIsRed] = useState(false)
+
+    useEffect(() => {
+        const timer = setTimeout(() => setTime(new Date()), 2000)
+        return () => clearTimeout(timer); // equivalent to componentDidMount
+    })
+
+
+    return(
+        <div>
+            <h1
+                onClick={() => setIsRed(!isRed)}
+                style={{color: isRed ? "red" : "orange"}}
+            >Use Effect Example</h1>
+            <h1>Example: {time.toLocaleTimeString()}</h1>
+        </div>
+    )
+
+}
+export default EffectComponent;
